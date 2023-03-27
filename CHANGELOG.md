1c778f8ba5f91270607aeb8a61aff3cca35a01b4
chaimain <cpriyadarshi@ifae.es>
2023-03-27 22:13:51 +0200

    Unify reading of energy axis into a single function. Have the option to pass custom energy edges for spectral flux estimation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Generalize usage of GeomConfig and Models for analysis with fermipy files by [@chaimain](https://github.com/chaimain).
 - Add support for common data types for different instruments by [@chaimain](https://github.com/chaimain).
 - Add support for selecting various spectral model parameters in a given Field of View by [@chaimain](https://github.com/chaimain).
+- Generalize reading energy axes by [@chaimain](https://github.com/chaimain).
 
 ## [0.1](https://github.com/chaimain/asgardpy/releases/tag/v0.1) - 2023-02-16
 
--- asgardpy/data/geom.py
+++ asgardpy/data/geom.py
@@ -6,6 +6,7 @@ from enum import Enum
 from typing import List
 
 from astropy import units as u
+from gammapy.maps import MapAxis
 
 from asgardpy.data.base import AngleType, BaseConfig, EnergyType, FrameEnum
 
@@ -21,6 +22,7 @@ __all__ = [
     "ProjectionEnum",
     "WcsConfig",
     "GeomConfig",
+    "get_energy_axis",
 ]
 
 
@@ -42,15 +44,18 @@ class EnergyAxisConfig(BaseConfig):
     min: EnergyType = 1 * u.GeV
     max: EnergyType = 1 * u.TeV
     nbins: int = 5
+    per_decade: bool = True
 
 
 class EnergyEdgesCustomConfig(BaseConfig):
-    edges: List[EnergyType] = []
+    edges: List[float] = []
+    unit: str = "TeV"
 
 
 class MapAxesConfig(BaseConfig):
     name: str = "energy"
     axis: EnergyAxisConfig = EnergyAxisConfig()
+    axis_custom: EnergyEdgesCustomConfig = EnergyEdgesCustomConfig()
 
 
 class SelectionConfig(BaseConfig):
@@ -86,3 +91,26 @@ class GeomConfig(BaseConfig):
     selection: SelectionConfig = SelectionConfig()
     axes: List[MapAxesConfig] = [MapAxesConfig()]
     from_events_file: bool = True
+
+
+def get_energy_axis(axes_config, only_edges=False, custom_range=False):
+    """
+    Read from a MapAxesConfig section to return the required energy axis/range.
+    """
+    energy_axis = axes_config.axis
+    energy_axis_custom = axes_config.axis_custom
+
+    if custom_range:
+        energy_range = energy_axis_custom.edges * u.Unit(energy_axis_custom.unit)
+    else:
+        energy_range = MapAxis.from_energy_bounds(
+            energy_min=u.Quantity(energy_axis.min),
+            energy_max=u.Quantity(energy_axis.max),
+            nbin=int(energy_axis.nbins),
+            per_decade=energy_axis.per_decade,
+            name=energy_axis.name,
+        )
+        if only_edges:
+            energy_range = energy_range.edges
+
+    return energy_range
