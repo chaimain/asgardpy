4e4a7e0f24a9cc8f636c0ebb437a797ae6846798
chaimain <cpriyadarshi@ifae.es>
2023-08-26 21:18:30 +0200

    Check the data paths for running the shell script

--- download_asgardpy_data.sh
+++ download_asgardpy_data.sh
@@ -2,9 +2,12 @@
 
 set  -eu
 
-#ASGARDPY_DATA=${ASGARDPY_DATA:-""}
+ASGARDPY_DATA=${ASGARDPY_DATA:-""}
+GAMMAPY_DATA=${GAMMAPY_DATA:-""}
 
 OUT_ZIP="fermipy_crab_zipped.zip"
+
+echo "{$GAMMAPY_DATA} is the path to Gammapy datasets"
 
 #curl \
 #  "$ASGARDPY_DATA"  \
