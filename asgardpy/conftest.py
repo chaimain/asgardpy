5dabcc14b093e6bb80485b4730836b13026401bb
chaimain <cpriyadarshi@ifae.es>
2023-08-27 11:06:03 +0200

    Run isort and black

--- asgardpy/conftest.py
+++ asgardpy/conftest.py
@@ -1,6 +1,6 @@
 import os
-import pytest
 
+import pytest
 from gammapy.utils.check import check_tutorials_setup
 
 
--- asgardpy/data/tests/test_dataset3d.py
+++ asgardpy/data/tests/test_dataset3d.py
@@ -1,11 +1,10 @@
-import os
-
 import pytest
+
 from asgardpy.analysis import AsgardpyAnalysis
+
 
 @pytest.mark.test_data
 def test_dataset3d_full(base_config):
-
     analysis = AsgardpyAnalysis(base_config)
 
     analysis.run(["datasets-3d"])
@@ -15,7 +14,6 @@ def test_dataset3d_full(base_config):
 
 @pytest.mark.test_data
 def test_dataset3d_different_config(base_config):
-
     analysis = AsgardpyAnalysis(base_config)
 
     analysis.config.target.from_3d = True
