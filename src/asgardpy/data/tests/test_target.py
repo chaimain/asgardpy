720d0ef10b8b05fa5832418b14a166e155c42acc
chaimain <cpriyadarshi@ifae.es>
2023-09-29 13:13:40 +0200

    Update some minor test coverages

--- src/asgardpy/data/tests/test_target.py
+++ src/asgardpy/data/tests/test_target.py
@@ -5,7 +5,7 @@ def test_models_from_config():
     """Test reading models from asgardpy config."""
 
     from asgardpy.config.generator import AsgardpyConfig, get_model_template
-    from asgardpy.data.target import read_models_from_asgardpy_config
+    from asgardpy.data.target import read_models_from_asgardpy_config, set_models
 
     config_eclp = AsgardpyConfig.read(get_model_template("eclp"))
     config_bpl2 = AsgardpyConfig.read(get_model_template("bpl2"))
@@ -28,6 +28,10 @@ def test_models_from_config():
     assert model_fov[0].spectral_model.tag[0] == "PowerLawNormSpectralModel"
     assert model_fov[0].spatial_model.tag[0] == "ConstantSpatialModel"
 
+    # Exception for empty models information in config.
+    with pytest.raises(Exception):
+        _, _ = set_models()
+
 
 @pytest.mark.test_data
 def test_set_models(base_config, gammapy_data_path):
@@ -37,26 +41,53 @@ def test_set_models(base_config, gammapy_data_path):
     from asgardpy.base.base import PathType
     from asgardpy.data.target import set_models
 
-    analysis = AsgardpyAnalysis(base_config)
-    analysis.run(["datasets-3d"])
+    analysis_0 = AsgardpyAnalysis(base_config)
+    analysis_1 = AsgardpyAnalysis(base_config)
 
-    analysis.config.target.source_name = "Crab Nebula"
-    analysis.config.target.components[
+    # Check when using create_source_skymodel function
+    analysis_0.run(["datasets-3d"])
+    # Check when using read_models_from_asgardpy_config
+    analysis_1.run(["datasets-1d"])
+
+    analysis_0.config.target.source_name = "Crab Nebula"
+    analysis_1.config.target.source_name = "Crab Nebula"
+
+    ebl_file = f"{gammapy_data_path}ebl/ebl_franceschini_2017.fits.gz"
+    model_file_0 = f"{gammapy_data_path}fermi-3fhl-crab/Fermi-LAT-3FHL_models.yaml"
+    model_file_1 = f"{gammapy_data_path}fermi-3fhl-crab/Fermi-LAT-3FHL_datasets.yaml"
+
+    analysis_0.config.target.components[
         0
-    ].spectral.ebl_abs.filename = f"{gammapy_data_path}ebl/ebl_franceschini_2017.fits.gz"
+    ].spectral.ebl_abs.filename = ebl_file
+    analysis_1.config.target.components[
+        0
+    ].spectral.ebl_abs.filename = ebl_file
 
     data_0, model_0 = set_models(
-        analysis.config.target,
-        analysis.datasets,
+        analysis_0.config.target,
+        analysis_0.datasets,
         datasets_name_list=None,
-        models=PathType(f"{gammapy_data_path}fermi-3fhl-crab/Fermi-LAT-3FHL_models.yaml"),
+        models=PathType(model_file_0),
+    )
+
+    data_1, model_1 = set_models(
+        analysis_0.config.target,
+        analysis_0.datasets,
+        datasets_name_list=None,
+    )
+    data_2, model_2 = set_models(
+        analysis_1.config.target,
+        analysis_1.datasets,
+        datasets_name_list=None,
     )
 
     with pytest.raises(TypeError):
         _, _ = set_models(
-            analysis.config.target,
-            analysis.datasets,
+            analysis_0.config.target,
+            analysis_0.datasets,
             datasets_name_list=None,
-            models=f"{gammapy_data_path}fermi-3fhl-crab/Fermi-LAT-3FHL_datasets.yaml",
+            models=model_file_1,
         )
     assert model_0[0].datasets_names == ["Fermi-LAT_00", "Fermi-LAT_01"]
+    assert str(model_1[0].spectral_model.model2.filename) == ebl_file
+    assert str(model_2[0].spectral_model.model2.filename) == ebl_file
