e037f4cf38e5b76238f06cffd11337b15da54673
chaimain <cpriyadarshi@ifae.es>
2024-03-11 09:39:50 +0100

    Revert "Remove pivot energy related code and update setting model amplitude _is_norm as per gammapy for the custom models"
    This reverts commit 755d2e751e0c292aa42d6bbea5cd6b247f65114a.

--- src/asgardpy/stats/__init__.py
+++ src/asgardpy/stats/__init__.py
@@ -4,10 +4,10 @@ Statistics Module
 # order matters to prevent circular imports
 isort:skip_file
 """
-
 from asgardpy.stats.stats import (
     check_model_preference_aic,
     check_model_preference_lrt,
+    fetch_pivot_energy,
     get_chi2_sig_pval,
     get_goodness_of_fit_stats,
     get_ts_target,
@@ -16,6 +16,7 @@ from asgardpy.stats.stats import (
 __all__ = [
     "check_model_preference_aic",
     "check_model_preference_lrt",
+    "fetch_pivot_energy",
     "get_chi2_sig_pval",
     "get_goodness_of_fit_stats",
     "get_ts_target",
